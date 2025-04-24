b3b80f9fd1ff27bd16c545cce44dcbdd256ba552
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2025-04-24 15:50:09 +0530

    design done but hero section is not responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import profile from '../assets/profile.png';
+import '../index.css'; // Make sure your scroll animation CSS is here
 
 const Hero = () => {
   return (
@@ -15,24 +16,41 @@ const Hero = () => {
         className="absolute left-[9%] top-1/2 transform -translate-y-1/2 w-52 h-52 md:w-72 md:h-72 rounded-full object-cover shadow-lg border-4 border-primary"
       />
 
+
+
+
+
+
       {/* Text Content */}
       <div className="z-10 ml-[35%] text-left">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 text-primary">
-          Hey, I'm Atharva 👋
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 text-offftext">
+          Hey, I'm <span className="text-transparent" style={{ WebkitTextStroke: '1px #fdf6e3' }}>Atharva</span>  👋
         </h1>
-        <p className="text-xl md:text-2xl mb-8 text-textLight max-w-2xl">
-          A passionate Web Developer crafting sleek, responsive websites using React, Tailwind CSS, and modern tech.
-        </p>
+        <div className="text-xl md:text-2xl mb-8 text-textLight max-w-2xl">
+          I am into{" "}
+          <span className="inline-block h-10 relative overflow-hidden">
+  <ul className="animate-scrollText leading-10 text-accent font-semibold text-2xl font-poppins">
+
+              {["Web Development", "UI/UX Design", "Data Analytics", "App Development"].map((item, i) => (
+                <li key={i} className="h-6 text-center">{item}</li>
+              ))}
+            </ul>
+          </span>
+        </div>
+
+
+
+        
         <div className="flex gap-6">
           <a
             href="#projects"
-            className="bg-primary text-background px-6 py-3 rounded-2xl text-lg font-medium shadow-md hover:scale-105 transition duration-300"
+            className="bg-offyellow text-background px-6 py-3 rounded-2xl text-lg font-medium shadow-md hover:scale-105 transition duration-300"
           >
-            View Projects
+            Download Resume
           </a>
           <a
             href="#contact"
-            className="border-2 border-primary px-6 py-3 rounded-2xl text-lg font-medium hover:bg-primary hover:text-background transition duration-300"
+            className="border-2 border-primary px-6 py-3 rounded-2xl text-lg font-medium hover:bg-offyellow hover:text-background transition duration-300"
           >
             Contact Me
           </a>
